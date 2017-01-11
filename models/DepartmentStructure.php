cec1c82cf93b28af07ceeb27975b13932e647da2
manservell <chappi@i.ua>
2017-01-11 21:02:09 +0200

    Создал с помощью gii модель для таблицы Sector и для DepartmentStructure, подключив эти два класса в сайтконтроллере, строчки подключения выделены серым шрифтом (как будто показывает, что-оно не активно или просто что-то не так ). А текст названия вкладок созданных можелей поему-то красный, и в дереве проекта тоже.+

---
+++ models/DepartmentStructure.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "department_structure".
+ *
+ * @property string $id
+ * @property string $structure_category
+ */
+class DepartmentStructure extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'department_structure';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['structure_category'], 'required'],
+            [['structure_category'], 'string', 'max' => 55],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'structure_category' => 'Structure Category',
+        ];
+    }
+}
