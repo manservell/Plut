ff3ab3655fb839d7c8d0376b456d9f4d59194cb3
manservell <chappi@i.ua>
2017-02-09 21:51:51 +0200

    cearch for sector

--- views/sector/create.php
+++ views/sector/create.php
@@ -6,8 +6,8 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model app\models\Sector */
 
-$this->title = Yii::t('app', 'Создать сектор');
-$this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Сектора'), 'url' => ['index']];
+$this->title = Yii::t('app', 'Create Sector');
+$this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Sectors'), 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="sector-create">
